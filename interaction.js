494d98657bae731d03ee054457c7985eb3a50805
zharmakhan-zn <78794633+zharmakhan-zn@users.noreply.github.com>
2021-02-10 04:18:28 +0400

    all details of my website

---
+++ interaction.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("navigation").style.width = "150px";
+  document.getElementById("main").style.marginLeft = "150px";
+}
+
+function closeNav() {
+  document.getElementById("navigation").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
